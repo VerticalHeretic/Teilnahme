1bc79868ea577d8341592d406bd652cd83f04637
Łukasz Stachnik <ls.warss98@gmail.com>
2024-11-17 13:02:49 +0100

    Adding SQLLite connection to the program, and migrating Students to sqlmodel. Additonally adding new dependecy

--- src/common/models.py
+++ src/common/models.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 from pydantic import BaseModel
 from enum import Enum
+from sqlmodel import SQLModel, Field
 
 class DegreeName(str, Enum):
     master = "Master"
     bachelor = "Bachelor"
 
-class BaseStudent(BaseModel):
+class BaseStudent(SQLModel):
     name: str
     surname: str
     degree: DegreeName
     semester: int
 
-class Student(BaseStudent):
-    id: int
+class Student(BaseStudent, table=True):
+    id: int = Field(default=None, primary_key=True)
 
 class BaseSubject(BaseModel):
     name: str
