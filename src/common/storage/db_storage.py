666287cc4634e56c3ff032da3726ab8718d2f254
Łukasz Stachnik <ls.warss98@gmail.com>
2024-11-17 14:10:42 +0100

    Creating new handler that is more SQL oriented but probably will work for other storages also. Testing it of course

--- src/common/storage/db_storage.py
+++ src/common/storage/db_storage.py
@@ -1,6 +1,7 @@
-from sqlmodel import SQLModel, create_engine, Session
-from typing import Annotated
+from sqlmodel import SQLModel, create_engine, Session, select
+from typing import Annotated, List, Type
 from fastapi import Depends
+from src.common.storage.storage import NewStorageHandler
 
 sqllite_file_name = "database.db"
 engine = create_engine(f"sqlite:///{sqllite_file_name}")
@@ -15,3 +16,42 @@ def create_db_and_tables():
 # Type alias for dependency injection of SQLModel Session using FastAPI's Depends
 # This allows us to inject database sessions into route handlers
 SessionDep = Annotated[Session, Depends(get_session)]
+
+class DBStorageHandler(NewStorageHandler):
+    
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_all(self, model_type: Type[SQLModel]) -> List[SQLModel]:
+        result = self.session.exec(select(model_type)).all()
+        return list(result)
+
+    def get_by_id(self, id: int, model_type: Type[SQLModel]) -> SQLModel:
+        return self.session.get(model_type, id)
+
+    def create(self, model: SQLModel) -> SQLModel:
+        self.session.add(model)
+        self.session.commit()
+        return model
+
+    def update(self, id: int, model: SQLModel) -> SQLModel:
+        db_model = self.session.get(type(model), id)
+        
+        if not db_model:
+            raise ValueError(f"{type(model).__name__} with id {id} not found")
+        
+        model_data = model.model_dump(exclude_unset=True)
+        db_model.sqlmodel_update(model_data)
+        self.session.add(db_model)
+        self.session.commit()
+        self.session.refresh(db_model)
+        return db_model
+
+    def delete(self, id: int, model_type: Type[SQLModel]) -> None:
+        db_model = self.get_by_id(id, model_type)
+
+        if not db_model:
+            raise ValueError(f"Model with id {id} not found")
+        
+        self.session.delete(db_model)
+        self.session.commit()
---
+++ tests/unit/common/storage/test_db_storage.py
@@ -0,0 +1,104 @@
+from multiprocessing import Value
+import pytest
+from sqlmodel import SQLModel, create_engine, Session
+
+from src.common.storage.db_storage import DBStorageHandler
+from src.common.models import Student, DegreeName
+
+@pytest.fixture
+def test_db(): 
+    engine = create_engine("sqlite:///:memory:")
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+class TestDBStorageHandler:
+    def test_get_all(self, test_db):
+        # Given
+        for i in range(1, 4):
+            test_db.add(Student(name=f"John {i}", surname=f"Doe {i}", degree=DegreeName.bachelor, semester=i))
+        test_db.commit()
+        storage_handler = DBStorageHandler(session=test_db)
+        want = [Student(id=i, name=f"John {i}", surname=f"Doe {i}", degree=DegreeName.bachelor, semester=i) for i in range(1, 4)]
+
+        # When
+        got = storage_handler.get_all(Student)
+
+        # Then
+        assert got == want
+    
+    def test_get_all_empty(self, test_db):
+        storage_handler = DBStorageHandler(session=test_db)
+        assert storage_handler.get_all(Student) == []
+
+    def test_get_by_id(self, test_db):
+        # Given
+        student = Student(name="John", surname="Doe", degree=DegreeName.bachelor, semester=1)
+        test_db.add(student)
+        test_db.commit()
+        storage_handler = DBStorageHandler(session=test_db)
+    
+        # When
+        got = storage_handler.get_by_id(1, Student)
+
+        # Then
+        assert got == student
+    
+    def test_get_by_id_not_found(self, test_db):
+        storage_handler = DBStorageHandler(session=test_db)
+        assert storage_handler.get_by_id(1, Student) is None
+
+
+    def test_create(self, test_db):
+        # Given
+        student = Student(name="John", surname="Doe", degree=DegreeName.bachelor, semester=1)
+        storage_handler = DBStorageHandler(session=test_db)
+
+        # When
+        got = storage_handler.create(student)
+
+        # Then
+        assert got == student
+
+    def test_delete(self, test_db):
+        # Given
+        student = Student(name="John", surname="Doe", degree=DegreeName.bachelor, semester=1)
+        test_db.add(student)
+        test_db.commit()
+        storage_handler = DBStorageHandler(session=test_db)
+
+        # When
+        storage_handler.delete(1, Student)
+
+        # Then
+        assert storage_handler.get_by_id(1, Student) is None
+        
+    def test_delete_not_existing(self, test_db):
+        # Given
+        storage_handler = DBStorageHandler(session=test_db)
+        
+        # When/Then
+        with pytest.raises(ValueError):
+            storage_handler.delete(1, Student)    
+            
+    def test_update(self, test_db):
+        # Given
+        student = Student(name="John", surname="Doe", degree=DegreeName.bachelor, semester=1)
+        test_db.add(student)
+        test_db.commit()
+        storage_handler = DBStorageHandler(session=test_db) 
+
+        # When
+        got = storage_handler.update(1, Student(id=1, name="Jane", surname="Doe", degree=DegreeName.bachelor, semester=1))
+
+        # Then
+        assert got == Student(id=1, name="Jane", surname="Doe", degree=DegreeName.bachelor, semester=1)
+    
+    def test_update_not_existing(self, test_db):
+        # Given
+        storage_handler = DBStorageHandler(session=test_db)
+        
+        # When/Then
+        with pytest.raises(ValueError):
+            storage_handler.update(1, Student(id=1, name="John", surname="Doe", degree=DegreeName.bachelor, semester=1))    
+    
