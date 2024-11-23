09348f3ce67e1257101bb745e945f9a345e84e17
Łukasz Stachnik <ls.warss98@gmail.com>
2024-11-23 17:16:56 +0100

    Adding router for students and making it nice

--- src/server/server.py
+++ src/server/server.py
@@ -1,15 +1,9 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, status
-from fastapi.exceptions import HTTPException
+from fastapi import FastAPI
 
-from src.common.errors import SemesterError
-from src.common.models import DegreeName, Student
 from src.common.storage.db_storage import create_db_and_tables
-from src.modules.students_operations import (
-    StudentsOperationsDep,
-    StudentValidationError,
-)
+from src.server.routers import students
 
 
 @asynccontextmanager
@@ -20,50 +14,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# Include the students router
+app.include_router(students.router)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
-
-
-@app.get("/students/")
-async def get_students(students_operations: StudentsOperationsDep) -> list[Student]:
-    return students_operations.get_students()
-
-
-@app.get("/students/{degree_name}")
-async def get_students_in_degree(
-    students_operations: StudentsOperationsDep,
-    degree_name: DegreeName,
-    semester: int | None = None,
-) -> list[Student]:
-    try:
-        return students_operations.get_students_in_degree(degree_name, semester)
-    except SemesterError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail=str(e)
-        ) from e
-
-
-@app.post("/students/")
-async def add_student(
-    students_operations: StudentsOperationsDep, student: Student
-) -> Student:
-    try:
-        return students_operations.add_student(student)
-    except (StudentValidationError, SemesterError) as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail=str(e)
-        ) from e
-
-
-@app.put("/students/{student_id}", status_code=status.HTTP_200_OK)
-async def update_student(
-    students_operations: StudentsOperationsDep, student_id: int, student: Student
-) -> Student:
-    return students_operations.update_student(student_id, student)
-
-
-@app.delete("/students/{student_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_student(students_operations: StudentsOperationsDep, student_id: int):
-    students_operations.delete_student(student_id)
